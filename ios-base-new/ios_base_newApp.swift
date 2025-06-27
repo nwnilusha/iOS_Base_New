d9f3c20a2511ea9c12d04fbbc94913c40645398d
Nilusha Wimalasena <nw.nilusha@gmail.com>
2025-06-27 10:27:23 +0100

    Initial Commit

---
+++ ios-base-new/ios_base_newApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_base_newApp.swift
+//  ios-base-new
+//
+//  Created by Nilusha Niwanthaka Wimalasena on 27/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_base_newApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios-base-newUITests/ios_base_newUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ios_base_newUITestsLaunchTests.swift
+//  ios-base-newUITests
+//
+//  Created by Nilusha Niwanthaka Wimalasena on 27/6/25.
+//
+
+import XCTest
+
+final class ios_base_newUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
