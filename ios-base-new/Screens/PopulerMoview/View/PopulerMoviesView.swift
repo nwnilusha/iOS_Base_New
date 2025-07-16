7ddcdfe953493e00e3c4be8bac8de0cc3e5c09ca
Nilusha Wimalasena <nw.nilusha@gmail.com>
2025-07-16 22:37:31 +0100

    Introduce constants

--- ios-base-new/Screens/PopulerMoview/View/PopulerMoviesView.swift
+++ ios-base-new/Screens/PopulerMoview/View/PopulerMoviesView.swift
@@ -8,10 +8,6 @@
 import SwiftUI
 import Foundation
 
-struct Constants {
-    static let baseImageURL = "https://image.tmdb.org/t/p/w92"
-}
-
 struct PopulerMoviesView: View {
     
     @StateObject var viewModel: PopulerMoviesViewModel
